2cb8856ca9b8fa99345fc553cf0c18544b4f793f
Dotzooo <adam.hsu@gogolook.com.tw>
2023-12-15 14:32:14 +0800

    feat: dashboard page

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,7 +1,14 @@
+import '../../assets/components/admin/dashboard.scss';
+
 export default function AdminDashboard() {
-    return (
-        <div>
-            Dashboard
+  return (
+    <div className="dashboard px-4">
+      <div className="page-header row no-gutters py-4">
+        <div className="text-sm-left text-md-left mb-sm-0 col-12 col-sm-4 mb-3">
+          <span className="text-uppercase page-subtitle">Dashboard</span>
+          <h3 className="page-title">Blog Overview</h3>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
