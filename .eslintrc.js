34efee67caade8883f88048da548aaa8e6f4e383
Dotzooo <adam.hsu@gogolook.com.tw>
2023-12-18 11:18:14 +0800

    feat typescript and eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,28 +4,26 @@ module.exports = {
     es2021: true, // ES2021版本
   },
   extends: [
-    'eslint:recommended', // 使用 ESLint 的基本規則
-    'plugin:react/recommended', // 使用 react 的規則
-    // "plugin:prettier/recommended",
+    "eslint:recommended", // 使用 ESLint 的基本規則
+    "plugin:react/recommended", // 使用 react 的規則
+    "plugin:@typescript-eslint/recommended",
   ],
+  parser: "@typescript-eslint/parser",
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "react/prop-types": "off"
+      }
+    }
   ],
   parserOptions: {
-    ecmaVersion: 'latest', // 使用最新的 ECMAScript 版本
-    sourceType: 'module',
+    ecmaVersion: "latest", // 使用最新的 ECMAScript 版本
+    sourceType: "module",
   },
-  plugins: ['react'], // 啟用 react 插件
+  plugins: ["react", "@typescript-eslint"], // 啟用 react 插件
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
   },
 };
