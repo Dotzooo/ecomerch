8c5395f983fc3604a05ec52cf0531ae6d2172968
Dotzooo <adam.hsu@gogolook.com.tw>
2023-12-20 00:23:53 +0800

    feat orders page overview

---
+++ src/components/admin/Overview.tsx
@@ -0,0 +1,32 @@
+import "../../assets/components/admin/overview.scss";
+
+interface OverviewProps {
+  data: {
+    title: string;
+    nums: string;
+    className: string;
+    icon: React.ReactNode;
+  }[];
+}
+
+export default function Overview(props: OverviewProps) {
+  const { data } = props;
+
+  return (
+    <div className="overview">
+      <div className="row">
+        {data.map((item, index) => (
+          <div className="col-sm-6 col-lg-3" key={index}>
+            <div className="overview-card rounded">
+              <div className={`icon ${item.className}`}>{item.icon}</div>
+              <div className="content">
+                <p className="text-white m-0">{item.title}</p>
+                <p className="text-white m-0">{item.nums}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/admin/AdminOrders.tsx
+++ src/pages/admin/AdminOrders.tsx
@@ -1,9 +1,40 @@
-import Breadcrumb from '../../components/admin/Breadcrumb';
-
+import Breadcrumb from "../../components/admin/Breadcrumb";
+import Overview from "../../components/admin/Overview";
+import { FaRegFaceKissWinkHeart, FaRegFaceGrinHearts, FaRegFaceSadCry, FaRegFaceSmile } from "react-icons/fa6";
 
 export default function AdminOrder() {
-  return <div>
-    <Breadcrumb pageName='Orders' pageTitle='訂單管理'/>
-    後台訂單列表管理頁面
-  </div>;
+  const orderOverview = [
+    {
+      title: "New Order",
+      nums: "10000",
+      className: 'purple',
+      icon: <FaRegFaceKissWinkHeart />
+    },
+    {
+      title: "Completed Orders",
+      nums: "5K+",
+      className: 'green',
+      icon: <FaRegFaceGrinHearts />
+    },
+    {
+      title: "Cancelled Order",
+      nums: "446",
+      className: 'orange',
+      icon: <FaRegFaceSadCry />
+    },
+    {
+      title: "Positive Rating",
+      nums: "1.5K",
+      className: 'yellow',
+      icon: <FaRegFaceSmile />
+    },
+  ];
+
+  return (
+    <div>
+      <Breadcrumb pageName="Orders" pageTitle="訂單管理" />
+      後台訂單列表管理頁面
+      <Overview data={orderOverview} />
+    </div>
+  );
 }
