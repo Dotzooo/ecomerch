38ced13a957e696f5fe47aebd45c52b9f47cb409
Dotzooo <adam.hsu@gogolook.com.tw>
2023-12-22 11:47:43 +0800

    feat Pagination component

---
+++ src/components/admin/Pagination.tsx
@@ -0,0 +1,24 @@
+import Pagination from "react-bootstrap/Pagination";
+import { PaginationProps } from "../../interface/admin";
+import '../../assets/components/admin/pagination.scss';
+export default function PaginationComponent({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) {
+  return (
+    <Pagination className="px-4 py-3 m-0">
+      {[...Array(totalPages).keys()].map((page) => (
+        <Pagination.Item
+          key={page + 1}
+          active={page + 1 === currentPage}
+          onClick={() => onPageChange(page + 1)}
+        >
+          {page + 1}
+        </Pagination.Item>
+      ))}
+    </Pagination>
+  );
+}
+
+<style lang="scss"></style>;
